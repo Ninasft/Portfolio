75f51e08a7c4c9af7942762b681df6d81a51d5bf
Siti Fatimah Tuma'ninah <siti.fatimah05@ui.ac.id>
2025-12-29 08:24:00 +0700

    chore: add projects

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,16 @@ btn_sun.addEventListener("click", () => {
   btn_sun.querySelector(".moon").style.display = document.body.classList.contains("dark-mode") ? "inline" : "none";
 });
 
+const btn_notify = document.querySelector(".alert-button");
+btn_notify.addEventListener("click", () => {
+  alert("Berhasil! Anda akan mendapatkan notifikasi ketika development selesai");
+});
+
+const btn_email = document.querySelector(".contact-card.email");
+btn_email.addEventListener("click", () => {
+  const email = "fatimah28nina@gmail.com";
+  const subject = "Hello from Portfolio";
+  const body = "Hi Nina, I'd like to discuss a project with you.";
+  
+  window.location.href = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+});
