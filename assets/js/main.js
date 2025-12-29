668fd1efc04f898861f5c084970ae0cb76af44b7
Siti Fatimah Tuma'ninah <siti.fatimah05@ui.ac.id>
2025-12-29 10:01:08 +0700

    chore: add mobile nav

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,3 +19,40 @@ btn_email.addEventListener("click", () => {
   
   window.location.href = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
 });
+
+
+const headerbtn= document.querySelector('.header__bars');
+const mobileNav = document.querySelector('.mobile-nav');
+const mobileLinks = document.querySelectorAll('.mobile-nav__link');
+// State
+let isMobileNavOpen = false;
+
+headerbtn.addEventListener("click", () => {
+  isMobileNavOpen = !isMobileNavOpen;
+  if (isMobileNavOpen===true) { 
+    mobileNav.style.display = 'flex';
+    document.body.style.overflowY='hidden';
+  } else{
+    mobileNav.style.display = 'none';
+    document.body.style.overflowY='auto';
+  }
+  
+}
+);
+
+mobileLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    isMobileNavOpen = false; 
+    mobileNav.style.display='none';
+    document.body.style.overflowY='auto';
+  })
+})
+
+const mobileSun = document.querySelector(".mobile-nav__sun");
+mobileSun.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+  btn_sun.querySelector(".sun").style.display = document.body.classList.contains("dark-mode") ? "none" : "inline";
+  btn_sun.querySelector(".moon").style.display = document.body.classList.contains("dark-mode") ? "inline" : "none";
+});
+
+
